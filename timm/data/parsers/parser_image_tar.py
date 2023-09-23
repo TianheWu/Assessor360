768c5996d0655b6b67d321e977e8945bf38033c8
Guspan Tanadi <36249910+guspan-tanadi@users.noreply.github.com>
2023-09-23 13:15:25 +0700

    docs: bit misspell class name in comment

--- timm/data/parsers/parser_image_tar.py
+++ timm/data/parsers/parser_image_tar.py
@@ -1,7 +1,7 @@
 """ A dataset parser that reads single tarfile based datasets
 
 This parser can read datasets consisting if a single tarfile containing images.
-I am planning to deprecated it in favour of ParerImageInTar.
+I am planning to deprecated it in favour of ParserImageInTar.
 
 Hacked together by / Copyright 2020 Ross Wightman
 """
